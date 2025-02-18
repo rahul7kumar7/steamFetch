fa1a7959689860de1ce1529d0ee2defdb4ebdff4
rahul kumar <rhlkmr911@outlook.com>
2025-02-19 01:09:51 +0530

    option to open zoomed screenshot in new tab and new state added to check whether image is loaded or not

--- client/src/Pages/Components/Screenshots.jsx
+++ client/src/Pages/Components/Screenshots.jsx
@@ -3,20 +3,24 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import {Navigation} from "swiper/modules";
 import { IoIosCloseCircle } from "react-icons/io";
+import {useState} from "react";
+import {Link} from 'react-router-dom';
 
 export default function Screenshots({screenshots}) {
 
     console.log(`screenshots are ${screenshots}`);
 
+    const [imgSrc, setImgSrc] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
     const handleImage = (e) => {
-        const imgSrc = e.target.currentSrc;
+        setImgSrc(e.target.currentSrc.replace(".600x338", ""));
         const imgModal = document.getElementById("imgModal");
         const imgOverlay = document.getElementById("imgOverlay");
         const imgElement = document.getElementById("modalImage");
-        imgElement.src = imgSrc.replace(".600x338", "")
-        // .replace(".600x338", "");
         imgModal.classList.remove("hidden");
         imgOverlay.classList.remove("hidden");
+        imgElement.src = e.target.currentSrc.replace(".600x338", "")
     }
 
     document.body.addEventListener("keydown", function(e) {
@@ -32,6 +36,7 @@ export default function Screenshots({screenshots}) {
     const closeModal = () => {
         document.getElementById("imgModal").classList.add("hidden");
         document.getElementById("imgOverlay").classList.add("hidden");
+        setIsLoaded(false)
     };
 
 
@@ -53,7 +58,6 @@ export default function Screenshots({screenshots}) {
             ))}
         </Swiper>
 
-
     <div id="imgOverlay" className="hidden fixed top-0 left-0 w-full h-full bg-black opacity-90 z-40" onClick={closeModal} >
     </div>
 
@@ -66,13 +70,26 @@ export default function Screenshots({screenshots}) {
                  bg-white p-4 rounded-lg shadow-lg
                  z-50 flex flex-col items-center"
     >
-        <button
-            onClick={closeModal}
-            className="absolute top-1 right-1 cursor-pointer"
-        >
-            <IoIosCloseCircle className="h-5 w-5" />
-        </button>
-        <img id="modalImage" className="rounded-lg shadow-lg" />
+        {isLoaded && (
+            <button
+                onClick={closeModal}
+                className="absolute top-1 right-1 cursor-pointer"
+            >
+                <IoIosCloseCircle className="h-5 w-5" />
+            </button>
+        )}
+
+        <img id="modalImage" className="rounded-lg shadow-lg" onLoad={()=> setIsLoaded(true)} />
+        {isLoaded && (
+            <div className="flex ">
+                <a  href={imgSrc} target={"_blank"}>
+                    <p className="text-red-800 font-semibold hover:underline">View Full Size</p>
+                </a>
+            </div>
+        )}
+
+
+
     </div>
         </>
     )
